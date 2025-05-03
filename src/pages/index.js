653f3235110f009f11f518669d90682f8ca8ca10
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-03 15:00:02 -0700

    Import all page in Signle File

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Login from './auth/Login'
 import Register from './auth/Register'
 import Dashboard from './admin/Dashboard'
+import ForgotPassword from './auth/ForgotPassword'
 
 export {
-    Login, Register, Dashboard
+    Login, Register, Dashboard,ForgotPassword
 }
