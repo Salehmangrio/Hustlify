44074ba1174c86994d84460c6ae037046889dbe1
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-03 14:51:54 -0700

    Route Addition to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import {
   Route
 } from 'react-router-dom'
 import { MainLayout, ProtectedLayout } from './layouts'
-import { Dashboard, Login, Register } from './pages'
+import { Dashboard, ForgotPassword, Login, Register } from './pages'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />} >
         <Route path='login' element={<Login />} />
+        <Route path='forget' element={ <ForgotPassword/>} />
         <Route path='register' element={<Register />} />
         <Route element={<ProtectedLayout />}>
           <Route index element={<Dashboard />} />
