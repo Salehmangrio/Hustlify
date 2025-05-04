c8421628751056c4ab93e9de16d91f6ed0eb9c18
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-03 22:42:52 -0700

    Protects routes Layout

--- src/layouts/ProtectedLayout.jsx
+++ src/layouts/ProtectedLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 const ProtectedLayout = () => {
-    let isLogged = localStorage.getItem('isLogged');
+    let isLogged = JSON.parse(localStorage.getItem('isLogged'));
     return isLogged ? <Outlet /> : <Navigate to={'/login'} replace />
 }
 
