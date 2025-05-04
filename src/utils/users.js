560f59dc5670a19efd9c279bd490959120929d46
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-04 01:55:03 -0700

    Dummy users for test

---
+++ src/utils/users.js
@@ -0,0 +1,143 @@
+export const users = [
+    {
+      id: 1,
+      name: "Alice Johnson",
+      lastMessage: "See you tomorrow!",
+      time: "9:15 AM",
+      avatar: "https://i.pravatar.cc/150?img=1"
+    },
+    {
+      id: 2,
+      name: "Bob Smith",
+      lastMessage: "Let's catch up later.",
+      time: "10:42 AM",
+      avatar: "https://i.pravatar.cc/150?img=2"
+    },
+    {
+      id: 3,
+      name: "Carla Mendes",
+      lastMessage: "Sure, I'll check it out.",
+      time: "12:03 PM",
+      avatar: "https://i.pravatar.cc/150?img=3"
+    },
+    {
+      id: 4,
+      name: "Daniel Lee",
+      lastMessage: "That's awesome!",
+      time: "1:30 PM",
+      avatar: "https://i.pravatar.cc/150?img=4"
+    },
+    {
+      id: 5,
+      name: "Emily Davis",
+      lastMessage: "Thanks for the update.",
+      time: "2:15 PM",
+      avatar: "https://i.pravatar.cc/150?img=5"
+    },
+    {
+      id: 6,
+      name: "Frank Wilson",
+      lastMessage: "Can't talk now, busy.",
+      time: "3:40 PM",
+      avatar: "https://i.pravatar.cc/150?img=6"
+    },
+    {
+      id: 7,
+      name: "Grace Kim",
+      lastMessage: "I'll get back to you soon.",
+      time: "4:25 PM",
+      avatar: "https://i.pravatar.cc/150?img=7"
+    },
+    {
+      id: 8,
+      name: "Henry Carter",
+      lastMessage: "What do you think?",
+      time: "5:00 PM",
+      avatar: "https://i.pravatar.cc/150?img=8"
+    },
+    {
+      id: 9,
+      name: "Isabel Moore",
+      lastMessage: "Great, talk later!",
+      time: "5:45 PM",
+      avatar: "https://i.pravatar.cc/150?img=9"
+    },
+    {
+      id: 10,
+      name: "Jake Turner",
+      lastMessage: "Can you send the file?",
+      time: "6:20 PM",
+      avatar: "https://i.pravatar.cc/150?img=10"
+    },
+    {
+      id: 11,
+      name: "Kelly Brown",
+      lastMessage: "Love the design!",
+      time: "6:55 PM",
+      avatar: "https://i.pravatar.cc/150?img=11"
+    },
+    {
+      id: 12,
+      name: "Liam Green",
+      lastMessage: "Meet me at the cafe?",
+      time: "7:15 PM",
+      avatar: "https://i.pravatar.cc/150?img=12"
+    },
+    {
+      id: 13,
+      name: "Mia White",
+      lastMessage: "It's working perfectly.",
+      time: "7:45 PM",
+      avatar: "https://i.pravatar.cc/150?img=13"
+    },
+    {
+      id: 14,
+      name: "Nathan Young",
+      lastMessage: "Where are you?",
+      time: "8:10 PM",
+      avatar: "https://i.pravatar.cc/150?img=14"
+    },
+    {
+      id: 15,
+      name: "Olivia Scott",
+      lastMessage: "Got it, thanks!",
+      time: "8:30 PM",
+      avatar: "https://i.pravatar.cc/150?img=15"
+    },
+    {
+      id: 16,
+      name: "Peter Hall",
+      lastMessage: "I'll be late.",
+      time: "9:00 PM",
+      avatar: "https://i.pravatar.cc/150?img=16"
+    },
+    {
+      id: 17,
+      name: "Quinn Adams",
+      lastMessage: "Talk tomorrow!",
+      time: "9:30 PM",
+      avatar: "https://i.pravatar.cc/150?img=17"
+    },
+    {
+      id: 18,
+      name: "Rachel Evans",
+      lastMessage: "Can we reschedule?",
+      time: "10:00 PM",
+      avatar: "https://i.pravatar.cc/150?img=18"
+    },
+    {
+      id: 19,
+      name: "Sam Brooks",
+      lastMessage: "Yup, on it.",
+      time: "10:25 PM",
+      avatar: "https://i.pravatar.cc/150?img=19"
+    },
+    {
+      id: 20,
+      name: "Tina Harris",
+      lastMessage: "Thank you so much!",
+      time: "11:00 PM",
+      avatar: "https://i.pravatar.cc/150?img=20"
+    }
+  ];
+  
