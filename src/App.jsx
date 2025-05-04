9297dcb6ac7cb8333b93cec4ae35b4ce79e6abea
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-04 01:55:54 -0700

    Project Routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,27 @@ import {
   Route
 } from 'react-router-dom'
 import { MainLayout, ProtectedLayout } from './layouts'
-import { Dashboard, ForgotPassword, Login, Register, NotFound } from './pages'
+import { Dashboard, ForgotPassword, Login, Register, NotFound, ChatList, ChatRoom } from './pages'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />} >
+
+        {/* Authentication Routes */}
         <Route path='login' element={<Login />} />
-        <Route path='forget' element={<ForgotPassword />} />
         <Route path='register' element={<Register />} />
+        <Route path='forget' element={<ForgotPassword />} />
+
+        {/* Protected Routes (Only Authorized can see content) */}
         <Route element={<ProtectedLayout />}>
+
           <Route index element={<Dashboard />} />
+
+          <Route path='chat' element={<ChatList />}>
+            <Route path=':id' element={<ChatRoom />} />
+          </Route>
+
         </Route>
         <Route path='*' element={<NotFound />} />
       </Route>
