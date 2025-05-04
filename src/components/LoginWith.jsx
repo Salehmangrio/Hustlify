e282bc20c7f6274dd004e20c15e9682447252cab
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-03 22:38:58 -0700

    Login With Button UI

--- src/components/LoginWith.jsx
+++ src/components/LoginWith.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const LoginWith = ({ Icon, title }) => {
+const LoginWith = ({ Icon, title=''}) => {
     return (
-        <button className="flex items-center gap-2 justify-center text-white border border-gray-600 py-2 rounded-lg hover:text-indigo-400 transition">
-            <Icon size={20} /> {title}
+        <button className={`flex items-center gap-2 ${title==='' ? 'w-[100px px-4 hover:bg-gray-500':'hover:text-indigo-400'} justify-center text-white border border-gray-600 py-2 rounded-lg  transition`}>
+            <Icon size={title===''?22:20} /> {title}
         </button>
     )
 }
