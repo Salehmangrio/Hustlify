9d9232612273e6ca98322348cb5c43040f763c7b
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-04 11:37:47 -0700

    MainLayout is Root Layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { AuthProvider } from '../context/AuthContext'
+import {Navbar }from '../components'
 
 
 const MainLayout = () => {
     return (
         <AuthProvider>
+            <Navbar/>
             <Outlet />
         </AuthProvider>
     )
