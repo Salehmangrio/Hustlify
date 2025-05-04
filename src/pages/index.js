407f784a84a62bb353b907a1cc94a19b87b59eb9
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-04 01:57:31 -0700

    Importing & Exporting all Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import Register from './auth/Register'
 import Dashboard from './admin/Dashboard'
 import ForgotPassword from './auth/ForgotPassword'
 import NotFound from './notfound/NotFound'
+import ChatList from './chat/ChatList'
+import ChatRoom from './chat/ChatRoom'
 
 export {
-    Login, Register, Dashboard,ForgotPassword,NotFound
+    Login, Register, Dashboard, ForgotPassword,
+    NotFound, ChatList,ChatRoom
 }
