2df6bfb58ac15973832939db102618e680ee8952
Saleh Muhammad Mangrio <Salehmangrio114@gmail.com>
2025-05-04 04:30:24 -0700

    Importing & Exporting all Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,10 @@ import ForgotPassword from './auth/ForgotPassword'
 import NotFound from './notfound/NotFound'
 import ChatList from './chat/ChatList'
 import ChatRoom from './chat/ChatRoom'
+import Orders from './orders/Orders'
+import OrderDetails from './orders/OrderDetails'
 
 export {
     Login, Register, Dashboard, ForgotPassword,
-    NotFound, ChatList,ChatRoom
+    NotFound, ChatList,ChatRoom,Orders,OrderDetails
 }
