1c3df984b816dd0a5a1f0ab468184adb08608ee4
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-04 17:49:44 -0700

    Navbar UI Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const activeStyle= (({isActive})=>isActive?'text-yellow-400 font-bold font-serif':'font-light font-mono')
     return (
-        <div className=' md:h-[60px] border flex justify-between px-6 items-center bg-gradient-to-r from-gray-900 to-slate-900 text-white py-2'>
-            <h1 className='font-extrabold font-serif text-xl'>#HustLify</h1>
+        <div className=' md:h-[60px] border border-gray-600 flex justify-between px-6 items-center bg-gradient-to-r from-gray-900 to-slate-900 text-white py-2'>
+            <h1 className='font-extrabold font-serif text-xl'> <Link to={`/`}>#HustLify</Link></h1>
             <ul className='flex  gap-x-6'>
-                <li><Link to={`login`}>Login</Link></li>
-                <li><Link to={`register`}>Sign Up</Link></li>
-                <li><Link to={`chat`}>Chat</Link></li>
+                <li><NavLink className={activeStyle} to={`login`}>Login</NavLink></li>
+                <li><NavLink className={activeStyle} to={`register`}>Sign Up</NavLink></li>
+                <li><NavLink className={activeStyle} to={`order`}>Orders</NavLink></li>
+                <li><NavLink className={activeStyle} to={`chat`}>Chat</NavLink></li>
             </ul>
         </div>
     )
